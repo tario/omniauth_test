173c3fbf026f8d29912ed74d138f13a03ae4f628
Dario Seminara <darios3@gmail.com>
2012-02-13 10:55:47 -0300

    added code to allow user creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
   def create
-    
-    #require "pry"; binding.pry
-    render :text => request.env['rack.auth'].inspect
-#    @user = User.find_or_create_from_auth_hash(auth_hash)
- #   self.current_user = @user
-  #  redirect_to '/'
+    auth = auth_hash()
+    require "pry"; binding.pry
+    unless @auth = Authorization.find_from_hash(auth)
+      # Create a new user or add an auth to existing user, depending on
+      # whether there is already a user signed in.
+      @auth = Authorization.create_from_hash(auth, current_user)
+    end
+    # Log the authorizing user in.
+    self.current_user = @auth.user
+  
+    render :text => "Welcome, #{current_user.name}."
   end
 
   protected
