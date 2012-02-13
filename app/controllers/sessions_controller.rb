8363b4ca8176df773448854b8922643980793f1c
Dario Seminara <darios3@gmail.com>
2012-02-13 11:09:36 -0300

    fixed authorization and sessions controller to allow developer provider

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   def create
     auth = auth_hash()
-    require "pry"; binding.pry
     unless @auth = Authorization.find_from_hash(auth)
       # Create a new user or add an auth to existing user, depending on
       # whether there is already a user signed in.
       @auth = Authorization.create_from_hash(auth, current_user)
+      @auth.save
     end
     # Log the authorizing user in.
     self.current_user = @auth.user
--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,8 +1,16 @@
 class Authorization < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :user_id, :uid, :provider
-  validates_uniqueness_of :uid, :scope => :provider
+  validates_presence_of :user_id, :provider
 
+  validate :developer_provider_or_uid, :on => :create
+  def developer_provider_or_uid
+    unless uid
+      unless provider.to_s == 'developer'
+        errors.add(:uid, "can't be blank unless provider is 'developer'")
+      end
+    end
+  end
+  
   def self.find_from_hash(hash)
     find_by_provider_and_uid(hash['provider'], hash['uid'])
   end
